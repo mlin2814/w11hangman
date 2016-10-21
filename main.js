5c7a4dcf37e75e6f8f37798936a46a4958cb8cff
Matthew Lin <Matthew Lin>
2016-10-20 20:03:24 -0400

    Adjusted comments and minor tweaks of logic

--- main.js
+++ main.js
@@ -1,7 +1,3 @@
-//Sorry you had to look at this awful work from me, if you can even call it work. I'd lie to think of it as a dumpster fire.
-//This was the most mind-boggling assignment yet, and just made me mentally shut down with how incomprehensible it was.
-//I would literally have to be walked through every step to get close to understanding this. Way more time than anyone could reasonably take time to help me with.
-
 
 var inquirer = require('inquirer');
 var prompt = require('prompt');
